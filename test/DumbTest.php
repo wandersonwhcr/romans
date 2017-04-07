53778030f61fe00de8bb20f2e47d38decb548746
Wanderson <wandersonwhcr@gmail.com>
2017-04-07 16:37:11 -0300

    Initialize PHPUnit configuration

---
+++ test/DumbTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace RomansTest;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Simple Dumb TestCase
+ */
+class DumbTest extends TestCase
+{
+    /**
+     * Dumb Test
+     *
+     * @return void
+     */
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
