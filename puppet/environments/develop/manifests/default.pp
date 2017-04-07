a5dbb41ba93b528caf614bd7575c36565eb52cbb
Wanderson <wandersonwhcr@gmail.com>
2017-04-07 16:09:39 -0300

    Install PHP

--- puppet/environments/develop/manifests/default.pp
+++ puppet/environments/develop/manifests/default.pp
@@ -36,3 +36,23 @@ package { "git":
 package { "unzip":
     name => "unzip",
 }
+
+exec { "php : key":
+    creates => "/etc/apt/trusted.gpg.d/php.gpg",
+    command => "wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg",
+}
+
+file { "php : list":
+    path    => "/etc/apt/sources.list.d/php.list",
+    content => "deb https://packages.sury.org/php/ jessie main",
+    require => Exec["php : key"],
+    before  => Exec["apt-get : update"],
+}
+
+package { "php : cli":
+    name => "php-cli",
+    require => [
+        File["php : list"],
+        Exec["apt-get : update"],
+    ],
+}
