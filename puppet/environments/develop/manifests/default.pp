347ca141eb0b7d13a02f3e97267c7efcbfc23ca4
Wanderson <wandersonwhcr@gmail.com>
2017-04-07 15:45:51 -0300

    Execute apt-get with Puppet

---
+++ puppet/environments/develop/manifests/default.pp
@@ -0,0 +1,17 @@
+Exec {
+    path    => ["/usr/bin", "/bin", "/usr/sbin", "/sbin"],
+    timeout => 0,
+}
+
+Package {
+    require => Exec["apt-get : update"],
+    before  => Exec["apt-get : autoremove"],
+}
+
+exec { "apt-get : update":
+    command => "apt-get update",
+}
+
+exec { "apt-get : autoremove":
+    command => "apt-get autoremove -q -y",
+}
