3911a2f1bdb512597a396a5000200ca328d95e08
Wanderson <wandersonwhcr@gmail.com>
2017-04-07 17:56:08 -0300

    Include method to get all tokens available

--- src/Grammar/Grammar.php
+++ src/Grammar/Grammar.php
@@ -20,4 +20,28 @@ class Grammar
     const T_D  = 'D';
     const T_CM = 'CM';
     const T_M  = 'M';
+
+    /**
+     * Get Tokens
+     *
+     * @return array Tokens Available
+     */
+    public function getTokens() : array
+    {
+        return [
+            'T_I'  => self::T_I,
+            'T_IV' => self::T_IV,
+            'T_V'  => self::T_V,
+            'T_IX' => self::T_IX,
+            'T_X'  => self::T_X,
+            'T_XL' => self::T_XL,
+            'T_L'  => self::T_L,
+            'T_XC' => self::T_XC,
+            'T_C'  => self::T_C,
+            'T_CD' => self::T_CD,
+            'T_D'  => self::T_D,
+            'T_CM' => self::T_CM,
+            'T_M'  => self::T_M,
+        ];
+    }
 }
--- test/Grammar/GrammarTest.php
+++ test/Grammar/GrammarTest.php
@@ -12,33 +12,44 @@ use Romans\Grammar\Grammar;
 class GrammarTest extends TestCase
 {
     /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        $this->tokens = [
+            'T_I'  => 'I',
+            'T_IV' => 'IV',
+            'T_V'  => 'V',
+            'T_IX' => 'IX',
+            'T_X'  => 'X',
+            'T_XL' => 'XL',
+            'T_L'  => 'L',
+            'T_XC' => 'XC',
+            'T_C'  => 'C',
+            'T_CD' => 'CD',
+            'T_D'  => 'D',
+            'T_CM' => 'CM',
+            'T_M'  => 'M',
+        ];
+    }
+
+    /**
      * Test Tokens
      */
     public function testTokens()
     {
         $reflection = new ReflectionClass(Grammar::class);
 
-        $numerals = [
-            'I',
-            'IV',
-            'V',
-            'IX',
-            'X',
-            'XL',
-            'L',
-            'XC',
-            'C',
-            'CD',
-            'D',
-            'CM',
-            'M',
-        ];
-
-        foreach ($numerals as $numeral) {
-            $token = 'T_' . $numeral;
-
+        foreach ($this->tokens as $token => $numeral) {
             $this->assertTrue($reflection->hasConstant($token));
             $this->assertSame($numeral, $reflection->getConstant($token));
         }
+    }
+
+    public function testAvailableTokens()
+    {
+        $grammar = new Grammar();
+
+        $this->assertSame($this->tokens, $grammar->getTokens());
     }
 }
