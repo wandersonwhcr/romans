a1e37728e6e222da5a1a79f0ca72a26657706f4f
Wanderson <wandersonwhcr@gmail.com>
2017-04-08 22:15:09 -0300

    Apply token values responsibility to Grammar
    Change responsibility from token values from Parser to Grammar. This
    modification enables the future Grammar changes, keeping Lexer and
    Parser processing unchanged.

--- src/Parser/Parser.php
+++ src/Parser/Parser.php
@@ -33,24 +33,6 @@ class Parser
     }
 
     /**
-     * Create a Mapper with Tokens and Values from Grammar
-     *
-     * @return array Grammar Token Values Mapper
-     */
-    protected function buildGrammarTokenValues() : array
-    {
-        $tokens = $this->getGrammar()->getTokens();
-        $values = [
-            1, 4, 5, 9,
-            10, 40, 50, 90,
-            100, 400, 500, 900,
-            1000,
-        ];
-
-        return array_combine($tokens, $values);
-    }
-
-    /**
      * Parse Tokens
      *
      * @param  string[] $tokens Grammar Tokens
@@ -58,7 +40,9 @@ class Parser
      */
     public function parse(array $tokens) : int
     {
-        $values    = $this->buildGrammarTokenValues();
+        $values          = $this->getGrammar()->getValues();
+        $tokensAvailable = array_flip($this->getGrammar()->getTokens());
+
         $result    = 0;
         $lastValue = null;
 
@@ -67,11 +51,11 @@ class Parser
                 throw new Exception(sprintf('Invalid token type "%s" at position %d', gettype($token), $position));
             }
 
-            if (! isset($values[$token])) {
+            if (! isset($tokensAvailable[$token])) {
                 throw new Exception(sprintf('Unknown token "%s" at position %d', $token, $position));
             }
 
-            $value = $values[$token];
+            $value = $values[$tokensAvailable[$token]];
 
             if (isset($lastValue) && $lastValue < $value) {
                 throw new Exception('Invalid Roman Number');
