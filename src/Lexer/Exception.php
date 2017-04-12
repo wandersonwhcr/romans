bf9b3a4944c492f4740b86aa5f0d1bf683bcbe20
Wanderson <wandersonwhcr@gmail.com>
2017-04-11 22:16:31 -0300

    Create position attribute into Lexer exception

--- src/Lexer/Exception.php
+++ src/Lexer/Exception.php
@@ -3,6 +3,7 @@
 namespace Romans\Lexer;
 
 use Exception as BaseException;
+use InvalidArgumentException;
 
 /**
  * Lexer Exception
@@ -10,4 +11,34 @@ use Exception as BaseException;
 class Exception extends BaseException
 {
     const UNKNOWN_TOKEN = 1;
+
+    private $position;
+
+    /**
+     * Set Position
+     *
+     * @param  int|null  $position Position Value
+     * @return self Fluent Interface
+     */
+    public function setPosition($position) : self
+    {
+        if (! (is_int($position) || is_null($position))) {
+            throw new InvalidArgumentException(
+                sprintf('Invalid $position type: "%s". Must be "int" or "null"', gettype($position))
+            );
+        }
+
+        $this->position = $position;
+        return $this;
+    }
+
+    /**
+     * Get Position
+     *
+     * @return int|null Position Value
+     */
+    public function getPosition()
+    {
+        return $this->position;
+    }
 }
